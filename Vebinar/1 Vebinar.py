6998dede003bf6c56478d3e50d292ba36fa51cc6
zhenya316 <56279967+zhenya316@users.noreply.github.com>
2024-07-19 03:18:13 +0300

    # Описание изменений

--- Vebinar/1 Vebinar.py
+++ Vebinar/1 Vebinar.py
@@ -1,32 +1,33 @@
 a = 20
 b = 12
 c = 1
-v= a*b - c
+v= a * b - c
 print(a+c)
 
 #списки
 a = [1,5, 3, 7,]
-print(max(a))
-print(min(a))
-print(len(a))
-print(sorted(a))
+print(max(a))       # max - максимальное значение списка
+print(min(a))       # min - минимальное значение списка
+print(len(a))       # len - длина списка
+print(sorted(a))    # Sorted - вывести сортированный список, он не изменился
 
 print(a)
-a.sort()
+a.sort()            # .sort - отсортировать список, он изменился
 print(a)
-a.reverse()
-print()
+a.reverse()         # .reverse - перевернуть список задом на перёд
+print(a)
 # Словари
 
-d = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4}
-keys = list(d.keys())
-value = list(d.values())
+d = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4}        #{key:value} - словарь
+keys = list(d.keys())           # .keys - вывести все ключи словаря
+value = list(d.values())        # .values - вывести все значения словаря
 print(keys)
 print(value)
 
-n = dict(zip(keys, value))
-m = list(zip(keys, value))
+n = dict(zip(keys, value))      #zip - соеденяет два списка в словарь по символу
+m = list(zip(keys, value))      #zip - соеденяет два списка в кортежи
 print(n)
 print(m)
+print(type(m))
 
-print(round(42.13 - 42, 2 ))         #round - округляет до определённых чисел после запятой
+print(round(42.13 - 42, 2 ))         #round - округляет до определённых чисел после запятой, второй число определяет количество
