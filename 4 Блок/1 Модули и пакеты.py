d74907e8d4caf8d2b03064d6d805fd49b916faa3
zhenya316 <56279967+zhenya316@users.noreply.github.com>
2024-08-20 20:26:49 +0300

    # Описание изменений

---
+++ 4 Блок/1 Модули и пакеты.py
@@ -0,0 +1,8 @@
+import module2 as m2
+from module1 import *       # импортировали всё из модуля1 *, и переменные и функции
+#from module2 import say_hi, main        # пример как выполнить импорт только определённых переменных или функций
+#say_hi()
+print(a)
+
+
+print(m2.__name__)
