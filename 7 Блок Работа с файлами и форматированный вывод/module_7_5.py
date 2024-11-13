62058585e952787cf42be2b8bd2b4dda3328f00d
zhenya316 <56279967+zhenya316@users.noreply.github.com>
2024-11-13 12:22:05 +0300

    # Описание изменений

--- 7 Блок Работа с файлами и форматированный вывод/module_7_5.py
+++ 7 Блок Работа с файлами и форматированный вывод/module_7_5.py
@@ -39,8 +39,8 @@ directory = os.getcwd()
 for root, dirs, files in os.walk(directory):
   for file in files:
     filepath = os.path.join(root, file)
-    filetime = os.path.getatime(file)
+    filetime = os.path.getmtime(filepath)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    filesize = os.path.getsize(file)
-    parent_dir = os.path.dirname(directory)
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(filepath)
     print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
